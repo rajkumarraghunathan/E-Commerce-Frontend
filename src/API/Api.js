037e61c005b2d7d7749cc1e014fd396f0932a5dd
Rajkumar <114281193+rajkumarraghunathan@users.noreply.github.com>
2023-12-20 20:36:50 +0530

    Latest commit

--- src/API/Api.js
+++ src/API/Api.js
@@ -1,6 +1,6 @@
-// const API_URL = "https://632fe3bf591935f3c886b34d.mockapi.io";
+
 // const API_URL = "http://localhost:5000";
-// const API_URL = "https://guvi-hackathon-2-backend.onrender.com";
+
 const API_URL = "https://e-commerce-backend-9kvr.onrender.com";
 
 
